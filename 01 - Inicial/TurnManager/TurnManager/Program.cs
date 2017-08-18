1099af224bed4168644431e54c014bf1a91382e9
Germán Küber <german.kuber@outlook.com>
2017-08-18 13:38:46 -0300

    Commit inicial

---
+++ 01 - Inicial/TurnManager/TurnManager/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TurnManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turnManager = new TurnManager(3);
+           
+           
+            while (true)
+            {
+                //Itero y leo lo que el usuario ingresa
+                var value = Console.ReadLine();
+
+                if (int.TryParse(value, out var parse))
+                {
+                    turnManager.Add(parse);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            while (!turnManager.IsEmpty())
+            {
+                //Mientras no sea Empty sigo iterando
+                Console.WriteLine(turnManager.Get());
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ 01 - Inicial/TurnManager/TurnManager/TurnManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace TurnManager
+{
+    public class TurnManager
+    {
+        private int[] _turnsList;
+        private int _last = 0;
+        public int Capacity { get; }
+        private int _totalCount;
+        public TurnManager() : this(capacity: 10)
+        {
+        }
+        public TurnManager(int capacity)
+        {
+            _turnsList = new int[capacity + 1];
+            Capacity = capacity;
+        }
+        public bool Add(int number)
+        {
+            if (_last < Capacity)
+            {
+                _turnsList[_last] = number;
+                ++_totalCount;
+                ++_last;
+                return true;
+            }
+            return false;
+        }
+        public int Get()
+        {
+            var returnData = _turnsList[0];
+            for (int i = 1; i < Capacity; i++)
+            {
+                _turnsList[i - 1] = _turnsList[i];
+            }
+            --_last;
+            return returnData;
+        }
+        public bool IsEmpty() => _last == 0;
+    }
+
+}
